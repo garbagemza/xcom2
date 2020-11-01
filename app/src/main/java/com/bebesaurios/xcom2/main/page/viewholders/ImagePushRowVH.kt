a6d2e4bb4d158786ad2ee3b659daff8b15adbc13
garbagemza <garbagemza@gmail.com>
2020-11-01 20:11:46 -0300

    Introducing Glide to update images from the server.
    More implementation to come.

--- app/src/main/java/com/bebesaurios/xcom2/main/page/viewholders/ImagePushRowVH.kt
+++ app/src/main/java/com/bebesaurios/xcom2/main/page/viewholders/ImagePushRowVH.kt
@@ -2,6 +2,7 @@ package com.bebesaurios.xcom2.main.page.viewholders
 
 import android.view.View
 import com.bebesaurios.xcom2.main.page.model.ImagePushRow
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.image_push_row.view.*
 
 
@@ -12,6 +13,9 @@ class ImagePushRowVH(val parent: View) : PageViewHolder<ImagePushRow>(parent) {
         parent.setOnClickListener {
             t.clickListener.invoke(t)
         }
+
+        Glide.with(parent).load("https://xcom-2.web.app/images/icon_grenadier.png").into(parent.imageView);
+
     }
 }
 
