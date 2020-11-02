45f38e96d41211edb445693271ae185e817ca7f6
garbagemza <garbagemza@gmail.com>
2020-11-02 09:45:39 -0300

    Changing Search view appearance and UX

--- app/src/main/java/com/bebesaurios/xcom2/search/SearchActivity.kt
+++ app/src/main/java/com/bebesaurios/xcom2/search/SearchActivity.kt
@@ -1,13 +1,19 @@
 package com.bebesaurios.xcom2.search
 
+import android.app.Activity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView.OnEditorActionListener
 import androidx.appcompat.app.AppCompatActivity
 import com.bebesaurios.xcom2.R
 import com.bebesaurios.xcom2.database.Repository
 import kotlinx.android.synthetic.main.search_activity.*
 import org.koin.android.ext.android.inject
+
 
 class SearchActivity : AppCompatActivity() {
 
@@ -17,7 +23,7 @@ class SearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.search_activity)
 
-        editText.addTextChangedListener(object: TextWatcher {
+        editText.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
             }
 
@@ -29,6 +35,16 @@ class SearchActivity : AppCompatActivity() {
                 updateModel(searchText)
             }
         })
+
+        editText.setOnEditorActionListener(OnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                hideKeyboard()
+                return@OnEditorActionListener true
+            }
+            false
+        })
+
+        editText.requestFocus()
     }
 
     private fun updateModel(searchText: String) {
@@ -38,5 +54,15 @@ class SearchActivity : AppCompatActivity() {
         recyclerView.adapter = adapter
     }
 
-
+}
+// TODO: Move to an extension class
+fun Activity.hideKeyboard() {
+    val imm: InputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    //Find the currently focused view, so we can grab the correct window token from it.
+    var view: View? = currentFocus
+    //If no view currently has focus, create a new one, just so we can grab a window token from it
+    if (view == null) {
+        view = View(this)
+    }
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
 }
